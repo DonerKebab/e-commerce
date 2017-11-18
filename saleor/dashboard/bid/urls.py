876660ce153fb2734a95da0febec6fd0ac7c248c
DonerKebab <phuong@compare52.com>
2017-11-19 00:05:22 +0700

    [ST33] Create next bid function

--- saleor/dashboard/bid/urls.py
+++ saleor/dashboard/bid/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^$', views.bid_list, name='bid-list'),
     url(r'^(?P<pk>[0-9]+)/$', views.bid_edit, name='bid-update'),
     url(r'^add/$', views.bid_edit, name='bid-add'),
+    url(r'^clone/(?P<pk>[0-9]+)/$', views.clone_bid, name='bid-clone'),
+    url(r'^(?P<pk>[0-9]+)/delete/$',
+        views.bid_delete, name='bid-delete'),
     ]
